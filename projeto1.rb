25fb05fde7c0b696c0e1bf5e6ca1d6ddc3638af4
alinemmonts <wyrd.dev@gmail.com>
2023-02-21 00:22:17 -0300

    first commit

---
+++ projeto1.rb
@@ -0,0 +1,10 @@
+print "Digite seu nome: "
+nome = gets.chomp
+
+print "Digite seu sobrenome: "
+sobrenome = gets.chomp
+
+print "Qual a sua idade? "
+idade = gets.chomp.to_i
+
+puts "Seu nome é #{nome} #{sobrenome}, e você tem #{idade} anos!"
